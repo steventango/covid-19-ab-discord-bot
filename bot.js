d9ccb5aef0776efa7c001f34ed261951c0bb5cb8
Steven Tang <TangSteven@users.noreply.github.com>
2020-07-23 16:25:23 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,63 +6,68 @@ var client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
+	client.user.setActivity('https://covid-19-ab-discord-bot.glitch.me/', {
+		type: 'PLAYING'
+	})
+	.then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+	.catch(console.error);
 });
 
 client.login(auth.token);
 
 client.on('message', async function (message) {
-    if (message.content.substring(0, 1) == '!') {
-        var args = message.content.substring(1).split(' ');
-        var cmd = args[0];
-       if (['help', 'info', 'information', 'cases', 'locations', 'recovery', 'recoveries', 'stats', 'statistics', 'testing'].indexOf(cmd) > -1) {
-        args = args.splice(1);
-        message.channel.startTyping();
-        switch(cmd) {
-            case 'help':
-                message.channel.send(new Discord.MessageEmbed()
-                .setColor('#00aad2')
-                .setTitle('Help')
-                .addFields(
-                    { name: '!info / !information', value: 'hyperlink to official website'},
-                    { name: '!cases / !locations', value: 'view number of cases and their locations'},
-                    { name: '!recovery / !recoveries', value: 'overview of recoveries in Alberta'},
-                    { name: '!stats / !statistics', value: 'hyperlink to official statistics website'},
-                    { name: '!testing', value: 'overview of testing in Alberta'}
-                ));
-                break;
-            case 'info':
-            case 'information':
-                message.channel.send(new Discord.MessageEmbed()
-                .setColor('#00aad2')
-                .setTitle('Information')
-                .setDescription('Hyperlink to offical website')
-                .setURL('https://www.alberta.ca/coronavirus-info-for-albertans.aspx')
-                );
-                break;
-            case 'cases':
-            case 'locations':
-                message.channel.send( await api.getLocationsEmbed());
-                break;
-            case 'testing':
-                message.channel.send( await api.getTestingEmbed());
-                break;
-            case 'recovery':
-            case 'recoveries':
-                message.channel.send( await api.getRecoveriesEmbed());
-                break;
-            case 'stats':
-            case 'statistics':
-                message.channel.send(new Discord.MessageEmbed()
-                .setColor('#00aad2')
-                .setTitle('Statistics')
-                .setDescription('Hyperlink to offical statistics website.')
-                .setURL('https://covid19stats.alberta.ca/')
-                );
-                break;
-        }
-        message.channel.stopTyping();
-       }
-     }
+	if (message.content.substring(0, 1) == '!') {
+		var args = message.content.substring(1).split(' ');
+		var cmd = args[0];
+		if (['help', 'info', 'information', 'cases', 'locations', 'recovery', 'recoveries', 'stats', 'statistics', 'testing'].indexOf(cmd) > -1) {
+			args = args.splice(1);
+			message.channel.startTyping();
+			switch (cmd) {
+				case 'help':
+					message.channel.send(new Discord.MessageEmbed()
+						.setColor('#00aad2')
+						.setTitle('Help')
+						.addFields(
+							{ name: '!info / !information', value: 'hyperlink to official website' },
+							{ name: '!cases / !locations', value: 'view number of cases and their locations' },
+							{ name: '!recovery / !recoveries', value: 'overview of recoveries in Alberta' },
+							{ name: '!stats / !statistics', value: 'hyperlink to official statistics website' },
+							{ name: '!testing', value: 'overview of testing in Alberta' }
+						));
+					break;
+				case 'info':
+				case 'information':
+					message.channel.send(new Discord.MessageEmbed()
+						.setColor('#00aad2')
+						.setTitle('Information')
+						.setDescription('Hyperlink to offical website')
+						.setURL('https://www.alberta.ca/coronavirus-info-for-albertans.aspx')
+					);
+					break;
+				case 'cases':
+				case 'locations':
+					message.channel.send(await api.getLocationsEmbed());
+					break;
+				case 'testing':
+					message.channel.send(await api.getTestingEmbed());
+					break;
+				case 'recovery':
+				case 'recoveries':
+					message.channel.send(await api.getRecoveriesEmbed());
+					break;
+				case 'stats':
+				case 'statistics':
+					message.channel.send(new Discord.MessageEmbed()
+						.setColor('#00aad2')
+						.setTitle('Statistics')
+						.setDescription('Hyperlink to offical statistics website.')
+						.setURL('https://covid19stats.alberta.ca/')
+					);
+					break;
+			}
+			message.channel.stopTyping();
+		}
+	}
 });
 
 //for glitch.com hosting
